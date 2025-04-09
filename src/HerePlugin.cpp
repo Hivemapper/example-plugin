d3a5e362314e96706886bf34a4ad58411bc83c70
derekhive <derek@hivemapper.com>
2025-04-08 17:46:20 -0700

    update timestamps

--- src/HerePlugin.cpp
+++ src/HerePlugin.cpp
@@ -40,15 +40,15 @@ int main(int argc, char *argv[]) {
         cout << "landmarks_3 size: " << landmarks_3.size() << endl;
 
         cout << "4) get landmarks since timestamp" << endl;
-        nlohmann::json landmarks_4 = get_landmarks_by_timestamp(1672531199);
+        nlohmann::json landmarks_4 = get_landmarks_by_timestamp(1744153670000);
         cout << "landmarks_4 size: " << landmarks_4.size() << endl;
 
         cout << "5) get landmarks since timestamp until timestamp" << endl;
-        nlohmann::json landmarks_5 = get_landmarks_by_timestamp(1672531199, 1672531200);
+        nlohmann::json landmarks_5 = get_landmarks_by_timestamp(1744153670000, 1744153671000);
         cout << "landmarks_5 size: " << landmarks_5.size() << endl;
 
         cout << "6) get landmarks until timestamp" << endl;
-        nlohmann::json landmarks_6 = get_landmarks_by_timestamp(-1, 1672531200);
+        nlohmann::json landmarks_6 = get_landmarks_by_timestamp(-1, 1744153669000);
         cout << "landmarks_6 size: " << landmarks_6.size() << endl;
 
 
@@ -56,13 +56,6 @@ int main(int argc, char *argv[]) {
         cout << "7) get landmarks after id" << endl;
         nlohmann::json landmarks_7 = get_landmarks_after_id(597);
         cout << "landmarks_7 size: " << landmarks_7.size() << endl;
-
-        // // get info
-        // cout << "Getting info data..." << endl;
-        // nlohmann::json info_data = get_json_from_url(INFO_URL);
-        // cout << "info data " << endl << info_data << endl;
-        // cout << "Downloaded info data: " << info_data.at("ssid").get<std::string>() << endl;
-        // cout << "Downloaded info data: " << info_data.at("api_version").get<std::string>() << endl;
 
         cout << "==============================================================" << endl;
         counter++;
