4802c54c6c33742fc52b305f4add96de8a8623af
derekhive <derek@hivemapper.com>
2025-06-04 18:14:31 -0700

    add docker version to load cache

---
+++ docker-load-cache/build-load-cache.sh
@@ -0,0 +1,5 @@
+docker run --rm --privileged tonistiigi/binfmt --install all
+docker buildx create --use --name mybuilder
+docker buildx inspect --bootstrap
+docker buildx build --progress=plain --platform linux/arm64 -t load-cache --build-arg CACHE_ARCHIVE=collection_2024050356.tar.gz --load .
+docker run -it --entrypoint /bin/bash load-cache
