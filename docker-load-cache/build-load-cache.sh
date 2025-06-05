6c9963c6a8175838c9f16a4f5724b9f3b3dd229f
derekhive <derek@hivemapper.com>
2025-06-04 18:55:48 -0700

    change name to collection

--- docker-load-cache/build-load-cache.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm --privileged tonistiigi/binfmt --install all
-docker buildx create --use --name mybuilder
-docker buildx inspect --bootstrap
-docker buildx build --progress=plain --platform linux/arm64 -t load-cache --build-arg CACHE_ARCHIVE=collection_2024050356.tar.gz --load .
-docker run --platform linux/arm64 -it load-cache
